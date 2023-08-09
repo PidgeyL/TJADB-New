9c060832e0da91767d1202805474245184467912
pidgey <pieter-jan@pidgey.net>
2023-08-09 10:02:46 +0200

    bugfix allowed-hosts

--- tjadb/settings.py
+++ tjadb/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = Config.server_secret
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [Config.hosts]
+ALLOWED_HOSTS = Config.hosts
 
 
 # Application definition
