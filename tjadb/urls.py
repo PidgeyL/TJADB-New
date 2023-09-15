fb4c7757269f518327e18389aa7de78b52919765
pidgey <pieter-jan@pidgey.net>
2023-09-15 22:28:07 +0200

    add audio preview

--- tjadb/urls.py
+++ tjadb/urls.py
@@ -48,4 +48,4 @@ urlpatterns = [
     path('donate/',                 website.donate),
 ]
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-#urlpatterns += static(settings.MEDIA_URL,  document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.MEDIA_URL,  document_root=settings.MEDIA_ROOT)
